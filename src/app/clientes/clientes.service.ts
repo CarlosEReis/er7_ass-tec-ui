570fa445a69e5cdc56ceaa06eb6def6871dc5308
CarlosEReis <carlos.er7@gmail.com>
2023-07-14 00:11:33 -0300

    feat: add metodos busca e pesquisa no service cliente

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, firstValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
@@ -15,5 +15,19 @@ export class ClientesService {
   public adicionar(cliente: any) : Promise<any> {
     const headers = new HttpHeaders().append('Content-type', 'Application/Json')
     return firstValueFrom(this.http.post(this.clientesURL, cliente, { headers }));
+  }
+
+  public pesquisar(nome: string) : Promise<any>{
+    if (nome) {
+      const params = new HttpParams().append('nome', nome);
+      return firstValueFrom(this.http.get(this.clientesURL, { params }))
+    }
+    return firstValueFrom(this.http.get(`${this.clientesURL}`));
+  }
+
+  public buscar(codigo: number): Promise<any> {
+    return firstValueFrom(
+      this.http.get(`${this.clientesURL}/${codigo}`)
+    );
   }
 }
