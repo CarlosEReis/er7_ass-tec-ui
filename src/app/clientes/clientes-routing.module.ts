4295061682d3a01d5c08a83f080538d9d18e2e5b
CarlosEReis <carlos.er7@gmail.com>
2023-07-14 00:14:01 -0300

    feat: add novas rotas no modulo clientes

--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClienteFormComponent } from './cliente-form/cliente-form.component';
+import { ClientesPesquisaComponent } from './clientes-pesquisa/clientes-pesquisa.component';
 
 const routes: Routes = [
-  { path: 'novo', component: ClienteFormComponent,
-  }
+  { path: '', component: ClientesPesquisaComponent },
+  { path: 'novo', component: ClienteFormComponent  },
+  { path: ':id/edicao', component: ClienteFormComponent }
 ];
 
 @NgModule({
