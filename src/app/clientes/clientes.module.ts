fbe026f65ce38ab2191c0e17c5eaafd226ad0bf0
CarlosEReis <carlos.er7@gmail.com>
2023-07-14 00:22:06 -0300

    feat: add novo componentes no modulo clientes

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -4,24 +4,28 @@ import { CommonModule } from '@angular/common';
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClienteFormComponent } from './cliente-form/cliente-form.component';
 import { PoFieldModule, PoModule, PoNotificationModule, PoTabsModule } from '@po-ui/ng-components';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ClientesPesquisaComponent } from './clientes-pesquisa/clientes-pesquisa.component';
 
 
 @NgModule({
   declarations: [
-    ClienteFormComponent
+    ClienteFormComponent,
+    ClientesPesquisaComponent
   ],
   imports: [
     CommonModule,
     ClientesRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     HttpClientModule,
 
     PoModule,
     PoFieldModule,
     PoTabsModule,
-    PoNotificationModule
+    PoNotificationModule,
+    PoTabsModule
   ]
 })
 export class ClientesModule { }
