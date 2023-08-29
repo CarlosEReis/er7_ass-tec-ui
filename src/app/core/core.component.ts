6d049e1b9f4a6d2971e3e8d6d5eeb99c2139a351
CarlosEReis <carlos.er7@gmail.com>
2023-08-29 19:14:53 -0300

    feat: add toolbar profile e dados do usuario logado

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { PoMenuItem } from '@po-ui/ng-components';
+import { Router } from '@angular/router';
+
+import { PoMenuItem, PoToolbarAction, PoToolbarProfile } from '@po-ui/ng-components';
+
 import { environment } from 'src/environments/environment.development';
+import { UsuarioService } from '../seguranca/usuario.service';
 
 @Component({
   selector: 'app-core',
@@ -10,14 +14,33 @@ import { environment } from 'src/environments/environment.development';
 export class CoreComponent {
 
   readonly appNomeDesc = environment.appNomeDesc;
+  user$ = this.usuarioService.retornaUsuario();
+  usuario = this.usuarioService.retornaUsuarioObj();
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) {}
+
+  profile: PoToolbarProfile = {
+    avatar: './assets/img/foto-carlos.jpg',
+    subtitle: this.usuario.sub+'',
+    title: this.usuario.user+''
+  };
+
+  readonly profileActions: Array<PoToolbarAction> = [
+    { icon: 'po-icon-exit', label: 'Sair', type: 'danger', separator: true, action: this.sair.bind(this)}
+  ];
 
   readonly menus: Array<PoMenuItem> = [
     { label: 'Home', link: '/', icon: 'po-icon po-icon-chart-area', shortLabel: 'Home' },
-    { label: 'Clientes', link: '/clientes', icon: 'po-icon po-icon-handshake', shortLabel: 'Clientes' },
-    { label: 'Chamados', link: '/chamados', icon: 'po-icon po-icon-news', shortLabel: 'Chamados' }
+    { label: 'Clientes', link: 'clientes', icon: 'po-icon po-icon-handshake', shortLabel: 'Clientes' },
+    { label: 'Chamados', link: 'chamados', icon: 'po-icon po-icon-news', shortLabel: 'Chamados' }
   ];
 
-  private onClick() {
-    alert('Clicked in menu item')
+  private sair() {  
+    this.usuarioService.logout();
+    this.router.navigate(['auth', 'login']);
   }
+
 }
