69ef3c8aac8dce3c5d4a1c7a3c289c4cf9b58799
CarlosEReis <carlos.er7@gmail.com>
2023-08-29 16:35:08 -0300

    feat: cria a rota de login no app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './guardas/autenticacao.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import("../app/core/core.module").then((m) => m.CoreModule) },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: 'app', loadChildren: () => import("../app/core/core.module").then((m) => m.CoreModule), canActivate: [ AutenticacaoGuard ]},
+  { path: 'auth/login', loadChildren: () => import("../app/seguranca/seguranca.module").then((m) => m.SegurancaModule) },
+  { path: '**', redirectTo: 'app', pathMatch: 'full' }
 ];
 
 @NgModule({
