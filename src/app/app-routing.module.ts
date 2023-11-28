ea9cec6934ac2aea53d4339e6af90564c481d3f2
CarlosEReis <carlos.er7@gmail.com>
2023-11-28 12:29:32 -0300

    feat: cria componente de acesso nao autorizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AutenticacaoGuard } from './guardas/autenticacao.guard';
+import { NaoAutorizadoComponent } from './core/nao-autorizado/nao-autorizado.component';
 
 const routes: Routes = [
   { path: 'app', loadChildren: () => import("../app/core/core.module").then((m) => m.CoreModule), canActivate: [ AutenticacaoGuard ]},
   { path: 'auth/login', loadChildren: () => import("../app/seguranca/seguranca.module").then((m) => m.SegurancaModule) },
+  { path: 'nao-autorizado', component: NaoAutorizadoComponent },
   { path: '**', redirectTo: 'app', pathMatch: 'full' }
 ];
 
