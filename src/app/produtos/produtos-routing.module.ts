fb28260cd0d4d5bb1741ae286fddec15fa283e98
CarlosEReis <carlos.er7@gmail.com>
2023-12-11 15:58:57 -0300

    feat: add rotas ao modulo de produtos

---
+++ src/app/produtos/produtos-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProdutosPesquisaComponent } from './produtos-pesquisa/produtos-pesquisa.component';
+import { AutenticacaoGuard } from '../guardas/autenticacao.guard';
+import { ProdutoFormComponent } from './produto-form/produto-form.component';
+
+const routes: Routes = [
+  { path: '', component: ProdutosPesquisaComponent, canActivate: [ AutenticacaoGuard ] },
+  { path: 'novo', component: ProdutoFormComponent, canActivate: [ AutenticacaoGuard ] },
+  { path: ':id', component: ProdutoFormComponent, data: { modoEdicao: false }, canActivate: [ AutenticacaoGuard] },
+  { path: ':id/edicao', component: ProdutoFormComponent, data: { modoEdicao: true }, canActivate: [ AutenticacaoGuard ],  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProdutosRoutingModule { }
