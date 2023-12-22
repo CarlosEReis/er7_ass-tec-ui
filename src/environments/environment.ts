5dd4cb0db286235916c980c92da6cf22386b43bd
CarlosEReis <carlos.er7@gmail.com>
2023-12-22 13:04:55 -0300

    build: ajusta arquivos environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: '',
     appNomeDesc: 'ER7 System: Sistema para gerenciamento de chamados técnicos.',
-    environment: 'Ambiente Online'
+    environment: 'Ambiente Produção'
 };
